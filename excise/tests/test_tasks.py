69c87e336d000ec274b30eacedcd0fbda890028c
Diwas Sharma <dsharma@thelabnyc.com>
2021-07-09 14:21:30 -0400

    gracefully handle errors in Excise API

--- excise/tests/test_tasks.py
+++ excise/tests/test_tasks.py
@@ -12,8 +12,79 @@ from ..utils import (
 from ..tasks import api_post_request_task
 
 
+config = AvataxConfiguration(
+    username_or_account="test",
+    password_or_license="test",
+    company_name="test",
+    use_sandbox=True,
+    autocommit=True,
+)
+
+
 @pytest.mark.vcr
-def test_api_post_request_task_sends_request(
+def test_api_post_request_task_with_invalid_productcodes(
+    product,
+    cigar_product_type,
+    order_with_lines,
+    address_usa_va,
+    shipping_zone,
+):
+
+    order_with_lines.shipping_address = address_usa_va
+    order_with_lines.shipping_method = shipping_zone.shipping_methods.get()
+    shipping_method = shipping_zone.shipping_methods.get()
+    shipping_method.price_amount = 0
+    shipping_method.save()
+    order_with_lines.shipping_method = shipping_method
+
+    product.product_type = cigar_product_type
+    product.save()
+
+    variant = product.variants.first()
+    variant.sku = "FAKEPROD"
+    variant.price_amount = Decimal(170)
+    variant.save()
+
+    for order_line in order_with_lines.lines.all():
+        order_line.product_name = product.name
+        order_line.variant_name = variant.name
+        order_line.product_sku = variant.sku
+        order_line.variant = variant
+        order_line.save()
+
+    order_with_lines.save()
+
+    request_data = get_order_request_data(order_with_lines, config)
+
+    base_url = get_api_url(config.use_sandbox)
+    transaction_url = urljoin(
+        base_url,
+        "AvaTaxExcise/transactions/create",
+    )
+    commit_url = urljoin(
+        base_url,
+        "AvaTaxExcise/transactions/{}/commit",
+    )
+
+    response = api_post_request_task(
+        transaction_url,
+        asdict(request_data),
+        asdict(config),
+        order_with_lines.id,
+        commit_url
+    )
+
+    assert response is None
+    msg = "Product \"FAKEPROD\" does not exist"
+
+    assert order_with_lines.events.count() == 1
+    event = order_with_lines.events.get()
+    assert event.type == OrderEvents.EXTERNAL_SERVICE_NOTIFICATION
+    assert msg in event.parameters["message"]
+
+
+@pytest.mark.vcr
+def test_api_post_request_task_with_valid_productcodes(
     product,
     cigar_product_type,
     order_with_lines,
@@ -44,14 +115,6 @@ def test_api_post_request_task_sends_request(
         order_line.save()
 
     order_with_lines.save()
-
-    config = AvataxConfiguration(
-        username_or_account="test",
-        password_or_license="test",
-        use_sandbox=True,
-        company_name="test",
-        autocommit=True,
-    )
 
     request_data = get_order_request_data(order_with_lines, config)
 
@@ -96,16 +159,10 @@ def test_api_post_request_task_order_doesnt_have_any_lines_with_taxes_to_calcula
 ):
     mock_api_post_request = {"error": {"message": "Wrong credentials"}}
     monkeypatch.setattr(
-        "saleor.plugins.avatax.tasks.api_post_request", lambda *_: mock_api_post_request
+        "saleor.plugins.avatax.tasks.api_post_request",
+        lambda *_: mock_api_post_request
     )
 
-    config = AvataxConfiguration(
-        username_or_account="test",
-        password_or_license="test",
-        use_sandbox=True,
-        company_name="test",
-        autocommit=False,
-    )
     request_data = {}
 
     base_url = get_api_url(config.use_sandbox)
